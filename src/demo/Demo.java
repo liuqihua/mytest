8fa45f4fe4620b9c7e2c20fe6192cdf0e4b5fec2
liuqihua <74597470+liuqihua@users.noreply.github.com>
2020-11-18 15:18:21 +0800

    Update Demo.java
    修改了·

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -10,4 +10,8 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("helloWord!");
     }
+    
+    
+    public void say(){
+        System.out.println("say");
 }
