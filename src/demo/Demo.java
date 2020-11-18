4c90227812b0fbbe4cd5c23e8cbdba208afdfc0e
admin <admin@qq.com>
2020-11-18 15:09:50 +0800

    第一次推送

---
+++ src/demo/Demo.java
@@ -0,0 +1,13 @@
+package demo;
+
+/**
+ * 功能描述:
+ *
+ * @author 刘启华
+ * @date 2020/11/18
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("helloWord!");
+    }
+}
