1b18da8941535df8119a4d7d5c62fd6f7d0125e7
admin <admin@qq.com>
2021-02-23 13:31:03 +0800

    测试了

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -10,6 +10,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("helloWord!");
         System.out.println("helloWord!");
+        System.out.println("helloWord!");
         say();
     }
     
