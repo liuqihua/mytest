5c41b2d2872899d2547b5532ef853cf37eb95551
admin <admin@qq.com>
2021-02-23 16:02:49 +0800

    添加一个类，修改一个类

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -8,14 +8,13 @@ package demo;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("helloWord!");
-        System.out.println("helloWord!");
-        System.out.println("helloWord!");
-        System.out.println("你好!");
-        System.out.println("你好!");
+
         say();
     }
-    
+    public static void hello(){
+        System.out.println("你好");
+    }
+
     
     public static void say() {
         System.out.println("say");
---
+++ src/demo/DemoTwo.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class DemoTwo {
+    public void fun2(){
+        System.out.println("你好方法二");
+    }
+
+}
